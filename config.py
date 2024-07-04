b9ca5df14cce60f043faea9f2443aa546fa3c4c6
Aman Gautam <129865249+amangautamdev@users.noreply.github.com>
2024-07-04 11:23:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,12 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) ACE 
-
 import os
 
-class Config(object):
-    # get a token from @BotFather
-    pass
-    """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7295982155:AAEPBhvrhe3lqRPDlE1733nB7bBX2bzOfFA")
-    #Your API ID from my.telegram.org
-    API_ID = 25919301
-#Your API Hash from my.telegram.org
-    API_HASH = os.environ.get("API_HASH", "1b4a6417a21802b7b3f4c152fda659b1")
+#Bot token @Botfather
+BOT_TOKEN = os.environ.get("BOT_TOKEN","7295982155:AAEPBhvrhe3lqRPDlE1733nB7bBX2bzOfFA")
 
-    AUTH_USERS = 5631563685
+#Your API ID from my.telegram.org
+API_ID = 25919301
+#Your API Hash from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "1b4a6417a21802b7b3f4c152fda659b1")
+
+AUTH_USERS = 5631563685
 
